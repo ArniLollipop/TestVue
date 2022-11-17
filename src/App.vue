c31c0f12207a5af1b153db9338a695da25d40daf
ArniLollipop <bocunahero@gmail.com>
2022-11-18 00:51:41 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="App">
+    <div class = "bg-[#F5F5F5]">
+      <Header></Header>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue'
+  export default{
+    name: "app",
+    components:{
+      Header,
+    },  
+  }
+</script>
