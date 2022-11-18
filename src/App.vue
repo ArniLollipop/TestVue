3a5e99f3afee1842ad817c172e25139e86d42dc4
ArniLollipop <bocunahero@gmail.com>
2022-11-19 00:37:13 +0600

    iwbufeowefobweof

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
         <Filters class = ""></Filters>
         <Filter_Button :button_class="buttons_class"/>
         <Filter_Text :text_class="text_class"/>
+        <List :list_class="list_class"/>
       </div>
   </div>
 </template>
@@ -16,6 +17,7 @@
   import Filters from './components/Filters.vue';
   import Filter_Button from './components/Filter_Button.vue';
   import Filter_Text from './components/Fliter_Text.vue';
+  import List from './components/List.vue';
 
   export default{
     name: "app",
@@ -25,6 +27,7 @@
       Filters,
       Filter_Button,
       Filter_Text,
+      List,
     },  
     data(){
       return{
@@ -41,6 +44,13 @@
           {class: "flex items-center justify-between pb-4 border-b border-gray w-full px-1"},
           {class: "flex items-center justify-between pb-4 border-b border-gray w-full px-1"},
           {class: "flex items-center justify-between pb-4 border-b border-gray w-full px-1"},
+        ],
+        list_class:[
+          {text_class:"text-[#42A5F5]", bg_class: "bg-[#42A5F5] w-1 h-[80%] absolute top-1/2 -translate-y-1/2 rounded-r-full"},
+          {text_class:"text-[#FF2E53]", bg_class: "bg-[#FF2E53] w-1 h-[80%] absolute top-1/2 -translate-y-1/2 rounded-r-full"},
+          {text_class:"text-[#219653]", bg_class: "bg-[#219653] w-1 h-[80%] absolute top-1/2 -translate-y-1/2 rounded-r-full"},
+          {text_class:"text-[#42A5F5]", bg_class: "bg-[#42A5F5] w-1 h-[80%] absolute top-1/2 -translate-y-1/2 rounded-r-full"},
+          {text_class:"text-[#42A5F5]", bg_class: "bg-[#9B51E0] w-1 h-[80%] absolute top-1/2 -translate-y-1/2 rounded-r-full"},
         ]
       }
     }
---
+++ src/components/List.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class = "container mx-auto px-5 py-4 mt-2">
+    <div class = "w-full" v-for="list in list_class" v-bind:key="list.text_class">
+      <div class = "w-full h-[50%] border-b border-gray relative mt-4 pb-2">
+        <div class = "">
+          <div :class="list.bg_class">
+          </div>
+        </div>
+        <div class = "grid grid-cols-11 ml-4">
+          <div class = "w-full col-span-6">
+            <span class = "font-bold text-xs text-[#686D7B]">Text</span>
+          </div>
+          <div class = "w-full col-span-2">
+            <span class = "font-bold text-xs text-[#686D7B]">Text</span>
+          </div>
+          <div class = "w-full col-span-1">
+            <span class = "font-bold text-xs text-[#686D7B]">Text</span>
+          </div>
+          <div class = "w-full col-span-1">
+            <span class = "font-bold text-xs text-[#686D7B]">Итого, $</span>
+          </div>
+        </div>
+        <div class = "grid grid-cols-11 ml-4">
+          <div class = "w-full col-span-6">
+            <span :class="list.text_class">#23</span>
+            <span class = "ml-4">Text</span>
+          </div>
+          <div class = "w-full col-span-2">
+            <span :class="list.text_class">Text</span>
+          </div>
+          <div class = "w-full col-span-1 font-semibold">
+            <span>00:</span>
+            <span :class="list.text_class">12:34</span>
+          </div>
+          <div class = "w-full col-span-1">
+            <span class = "font-semibold text-sm">2 340,0</span>
+          </div>
+        </div>
+        <div class = "grid grid-cols-11 mt-2 items-center ml-4">
+          <div class = "w-full col-span-6">
+            <span class = "text-sm text-[#686D7B]">Text</span>
+          </div>
+          <div class = "w-full cols-span-5">
+            <div class = "w-full flex items-center gap-x-2">
+              <div class = "bg-[#EBF2FF] rounded-md w-full col-span-1 flex items-center gap-x-2 px-2 py-1">
+                <div class = "h-2 w-2 bg-[#FFC805] rounded-full">
+
+                </div>
+                Metka
+              </div>
+              <div class = "bg-[#EBF2FF] rounded-md w-full col-span-1 flex items-center gap-x-2 px-2 py-1">
+                <div class = "h-2 w-2 bg-[#FFC805] rounded-full">
+
+                </div>
+                Metka
+              </div>
+              <div class = "bg-[#EBF2FF] rounded-md w-full col-span-1 flex items-center gap-x-2 px-2 py-1">
+                <div class = "h-2 w-2 bg-[#FFC805] rounded-full">
+
+                </div>
+                Metka
+              </div>
+              <div class = "bg-[#EBF2FF] rounded-md w-full col-span-1 flex items-center gap-x-2 px-2 py-1">
+                <div class = "h-2 w-2 bg-[#FFC805] rounded-full">
+
+                </div>
+                Metka
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class = "grid grid-cols-6 mt-4 gap-x-4 ml-4">
+          <div class = "col-span-1 w-full ">
+            <span class = "text-sm text-[#828282] ">Имя</span>
+          </div>
+          <div class = "col-span-1 w-full ">
+            <span class = "text-sm text-[#828282] ">Text</span>
+          </div>
+          <div class = "col-span-1 w-full ">
+            <span class = "text-sm text-[#828282] ">Text</span>
+          </div>
+          <div class = "col-span-1 w-full ">
+            <span class = "text-sm text-[#828282] ">Status:Text</span>
+          </div>
+          <div class = "col-span-1 w-full ">
+            <span class = "text-sm text-[#828282] ">Text</span>
+          </div>
+          <div class = "col-span-1 w-full ">
+            <span class = "text-sm text-[#828282] ">Имя</span>
+          </div>
+        </div>
+        <div class = "absolute top-2 right-2 flex items-center gap-x-5">
+          <Stop/>
+          <Dots/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Stop from '@/icons/Stop.vue';
+  import Dots from '@/icons/Dots.vue';
+  export default{
+    name:"App-List",
+    props:{
+      list_class:{},
+    },
+    components:{
+      Stop,
+      Dots,
+    }
+  }
+</script>
