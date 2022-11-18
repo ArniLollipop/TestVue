c3f8df00eb052648ba2e8da891ef0a3ee07727d5
ArniLollipop <bocunahero@gmail.com>
2022-11-19 00:43:40 +0600

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,25 @@
 <template>
   <div id="App">
     <Header></Header>
-<<<<<<< HEAD
       <Navbar/>
       <div class = "bg-[#F5F5F5] pb-2 mt-2 min-h-[88vh]"> 
         <Filters class = ""></Filters>
         <Filter_Button :button_class="buttons_class"/>
         <Filter_Text :text_class="text_class"/>
         <List :list_class="list_class"/>
+        <Todo v-for="item in todoList" :key="item.id" :todoList="item" />
       </div>
-=======
-    <Navbar />
-    <div class="bg-[#F5F5F5] pb-20 mt-2 min-h-[88vh]">
-      <Filters class=""></Filters>
-      <Filter_Button :button_class="buttons_class" />
-      <Filter_Text :text_class="text_class" />
-      <Todo v-for="item in todoList" :key="item.id" :todoList="item" />
-    </div>
->>>>>>> c4d48beda97078e169267b096c5bd1cef9d287ae
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
   import Header from './components/Header.vue'
   import Navbar from './components/Navbar.vue';
   import Filters from './components/Filters.vue';
   import Filter_Button from './components/Filter_Button.vue';
   import Filter_Text from './components/Fliter_Text.vue';
   import List from './components/List.vue';
+  import Todo from './components/Todo.vue';
 
   export default{
     name: "app",
@@ -39,6 +30,7 @@
       Filter_Button,
       Filter_Text,
       List,
+      Todo,
     },  
     data(){
       return{
@@ -62,82 +54,8 @@
           {text_class:"text-[#219653]", bg_class: "bg-[#219653] w-1 h-[80%] absolute top-1/2 -translate-y-1/2 rounded-r-full"},
           {text_class:"text-[#42A5F5]", bg_class: "bg-[#42A5F5] w-1 h-[80%] absolute top-1/2 -translate-y-1/2 rounded-r-full"},
           {text_class:"text-[#42A5F5]", bg_class: "bg-[#9B51E0] w-1 h-[80%] absolute top-1/2 -translate-y-1/2 rounded-r-full"},
-        ]
-      }
-    }
-  }
-=======
-import Header from "./components/Header.vue";
-import Navbar from "./components/Navbar.vue";
-import Filters from "./components/Filters.vue";
-import Filter_Button from "./components/Filter_Button.vue";
-import Todo from "./components/Todo.vue";
-import Filter_Text from "./components/Fliter_Text.vue";
-export default {
-  name: "app",
-  components: {
-    Header,
-    Navbar,
-    Filters,
-    Filter_Button,
-    Todo,
-    Filter_Text,
-  },
-  data() {
-    return {
-      buttons_class: [
-        {
-          alert_class: "hidden",
-          left_class: "w-1 z-10 h-full bg-[#F2994A]",
-          class:
-            "flex z-0 items-center overflow-hidden w-[57px] h-[36px] bg-[#42A5F5] rounded-md text-white",
-        },
-        {
-          alert_class: "hidden",
-          left_class: "w-1 z-10 h-full bg-[#08257A]",
-          class:
-            "flex z-0 items-center overflow-hidden w-[57px] h-[36px] bg-[#F2F2F2] rounded-md",
-        },
-        {
-          alert_class: "",
-          left_class: "w-1 z-10 h-full bg-[#9B51E0]",
-          class:
-            "flex z-0 items-center pr-2 overflow-hidden w-[89px] h-[36px] bg-[#F2F2F2] rounded-md",
-        },
-        {
-          alert_class: "hidden",
-          left_class: "w-1 z-10 h-full bg-[#27AE60]",
-          class:
-            "flex z-0 items-center overflow-hidden w-[57px] h-[36px] bg-[#F2F2F2] rounded-md",
-        },
-      ],
-      text_class: [
-        {
-          class:
-            "flex items-center justify-between pb-4 border-b border-[#42A5F5] w-full px-1",
-        },
-        {
-          class:
-            "flex items-center justify-between pb-4 border-b border-gray w-full px-1",
-        },
-        {
-          class:
-            "flex items-center justify-between pb-4 border-b border-gray w-full px-1",
-        },
-        {
-          class:
-            "flex items-center justify-between pb-4 border-b border-gray w-full px-1",
-        },
-        {
-          class:
-            "flex items-center justify-between pb-4 border-b border-gray w-full px-1",
-        },
-        {
-          class:
-            "flex items-center justify-between pb-4 border-b border-gray w-full px-1",
-        },
-      ],
-      todoList: [
+        ],
+        todoList: [
         {
           id: 1,
           isOpen: false,
@@ -185,10 +103,9 @@ export default {
           ],
         },
       ],
-    };
-  },
-};
->>>>>>> c4d48beda97078e169267b096c5bd1cef9d287ae
+      }
+    }
+  }
 </script>
 
 <style>
---
+++ src/components/Filter_Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class = "container mx-auto px-5  gap-x-2 ">
+    <div class = "w-1/4">
+      <div class = "grid grid-cols-9 gap-x-2">
+        <div v-for="classes in button_class" v-bind:key="classes.div_class" :class="classes.div_class">
+          <button :class = "classes.class">
+            <div :class = "classes.left_class">
+            </div>
+            <div class = "flex justify-center w-full">
+              <p class = ""> Text </p>
+            </div>
+            <Alert :class = "classes.alert_class"/>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+  import Alert from '@/icons/Alert.vue';
+  export default{
+    name : "App-Filter_Button",
+    props:{
+      button_class:{}
+    },
+    components:{
+      Alert,
+    }
+  }
+</script>
